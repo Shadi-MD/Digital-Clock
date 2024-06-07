a7c03b1d557c9c5f8cf377e3242db8339aaae502
Shadi <s.mirdavoudi24@gmail.com>
2024-06-07 16:04:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const $ = document;
 
-let hourSection = $.querySelector("#hour");
-let minutSection = $.querySelector("#minute");
-let secondSection = $.querySelector("#seconds");
+let hourSection = $("#hour");
+let minutSection = $("#minute");
+let secondSection = $("#seconds");
 
 function showTime() {
   let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
 
-  hourSection.innerHTML = hours;
-  if (hours < 10) {
-    hourSection.innerHTML = `0${hours}`;
-  }
-  minutSection.innerHTML = minutes;
-  if (minutes < 10) {
-    minutSection.innerHTML = `0${minutes}`;
-  }
-  secondSection.innerHTML = seconds;
-  if (seconds < 10) {
-    secondSection.innerHTML = `0${seconds}`;
-  }
+  let hours = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+  let minutes =
+    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  let seconds =
+    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
 
-  console.log(seconds);
+  // hourSection.innerHTML = hours;
+  // if (hours < 10) {
+  //   hourSection.innerHTML = `0${hours}`;
+  // }
+  // minutSection.innerHTML = minutes;
+  // if (minutes < 10) {
+  //   minutSection.innerHTML = `0${minutes}`;
+  // }
+  // secondSection.innerHTML = seconds;
+  // if (seconds < 10) {
+  //   secondSection.innerHTML = `0${seconds}`;
+  // }
+
+  // console.log(seconds);
 }
 
 setInterval(showTime, 1000);
-
